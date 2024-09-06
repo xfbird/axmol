14eeb4706fb7de1c1ff7d0d3d42fa08b188730de
halx99 <halx99@live.com>
2024-09-07 00:16:51 +0800

    Rename FileUtils::getFileExtension to getPathExtension

--- core/platform/Image.cpp
+++ core/platform/Image.cpp
@@ -1436,7 +1436,7 @@ bool Image::initWithTGAData(tImageTGA* tgaData)
 
     if (ret)
     {
-        if (FileUtils::getInstance()->getFileExtension(_filePath) != ".tga")
+        if (FileUtils::getPathExtension(_filePath) != ".tga")
         {
             AXLOGW("Image WARNING: the image file suffix is not tga, but parsed as a tga image file. FILE: {}",
                   _filePath);
@@ -2317,7 +2317,7 @@ bool Image::saveToFile(std::string_view filename, bool isToRGB)
         return false;
     }
 
-    std::string fileExtension = FileUtils::getInstance()->getFileExtension(filename);
+    std::string fileExtension = FileUtils::getPathExtension(filename);
 
     if (fileExtension == ".png")
     {
