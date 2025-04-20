a71d689fd57ed811509392ed11bda4c0f5effec6
xfbird <xfbird@126.com>
2025-04-20 12:06:58 +0800

    增加 Bit6EnDecode

---
+++ core/gameex/Bit6EnDecode.h
@@ -0,0 +1,17 @@
+
+#ifndef GMXXEXT_ENCODE6BITSTR_H
+    #define GMXXEXT_ENCODE6BITSTR_H
+    #include <string>
+    #include <string_view>
+    namespace ax
+    {
+        // 新增常量定义
+        namespace gameex
+        {
+            constexpr uint8_t BASE_OFFSET      = 0x3C;// 编码偏移量
+            constexpr uint8_t MAX_ENCODE_VALUE = 0x3F; // 6位最大值
+            std::string Encode6BitStr(std::string_view asmsg);
+            std::string Decode6BitStr(std::string_view asmsg);
+        } // namespace gameex
+    } // namespace ax
+#endif
